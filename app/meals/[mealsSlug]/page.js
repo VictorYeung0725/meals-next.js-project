0cbb650d28978ac5c2b143c7acc92ca5e89023a9
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-12-12 21:55:42 +0800

    finished on home page and routing

---
+++ app/meals/[mealsSlug]/page.js
@@ -0,0 +1,6 @@
+//http://localhost:3000/meals/ ... anything dynamic here will render
+import React from 'react';
+
+export default function MealsDetailsPage() {
+  return <div>Meals Details</div>;
+}
